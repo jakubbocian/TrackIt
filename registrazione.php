6f409431c2470b64c612a95324e37fd934677bb0
ZTDavide <dadocarto503@gmail.com>
2023-05-04 10:10:47 +0200

    Registrazione

---
+++ registrazione.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.111.3">
+  <title>Registrazione</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
+
+  <link href="css/bootstrap2.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="sign-in.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+  <main class="form-signin w-100 m-auto">
+    <form>
+      <img class="mb-4" src="images/pack.png" alt="" width="72" height="75">
+      <h1 class="h3 mb-3 fw-normal">Registrati</h1>
+
+
+      <div class="form-floating">
+        <input type="fname" class="form-control" id="floatingInput" placeholder="a">
+        <label for="floatingInput">Username</label>
+      </div>
+      <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-body-secondary">&copy; TrackIT 2017–2023</p>
+    </form>
+  </main>
+
+
+</body>
+
+</html>
