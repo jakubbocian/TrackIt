eade122e7a75f36095176b902e231f395d303f94
ZTDavide <dadocarto503@gmail.com>
2023-05-08 10:55:02 +0200

    Accesso cartella + tuoipacchi

--- accesso/registrazione.php
+++ accesso/registrazione.php
@@ -8,23 +8,30 @@
   <meta name="description" content="">
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Hugo 0.111.3">
-  <title>Login</title>
+  <title>Registrazione</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
 
-  <link href="css/bootstrap2.min.css" rel="stylesheet">
+  <link href="../css/bootstrap2.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="sign-in.css" rel="stylesheet">
 </head>
 
+
+
 <body class="text-center">
 
   <main class="form-signin w-100 m-auto">
     <form>
-     <a href = "index.html"><img class="mb-4" src="images/pack.png" alt="" width="72" height="75"></a>
-      <h1 class="h3 mb-3 fw-normal">Effettua il login</h1>
+      <a href = "../index.html"><img class="mb-4" src="../images/pack.png" alt="" width="72" height="75"></a>
+      <h1 class="h3 mb-3 fw-normal">Registrati</h1>
 
+
+      <div class="form-floating">
+        <input type="fname" class="form-control" id="floatingInput" placeholder="a">
+        <label for="floatingInput">Username</label>
+      </div>
       <div class="form-floating">
         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
         <label for="floatingInput">Email address</label>
@@ -35,7 +42,7 @@
       </div>
 
       <p class="mt-3 mb-3 text-body-secondary">
-        Non hai un acconunt? <a href="registrazione.php">Registrati qui</a>
+        Sei già registrato? <a href="login.php">Accedi qui</a>
       </p>
 
       <div class="checkbox mb-3">
