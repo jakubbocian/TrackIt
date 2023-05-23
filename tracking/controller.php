aa57836a754db7c09106b1141a67884e786eed5d
jakubbocian <jakub.bocian@mpolo.it>
2023-05-23 10:19:44 +0200

    resume commint

--- tracking/controller.php
+++ tracking/controller.php
@@ -1,13 +1,14 @@
 <?php
 include_once "../database.php";
 include_once "trackingsTemplate.php";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $carrier = $_POST["carrier"];
     $tracking = $_POST["tracking"];
 
     $res = safeQueryId("INSERT INTO tracking (name, carrier, tracking) VALUES (?, ?, ?)", array($name, $carrier, $tracking));
-    $userId = 1;
+    $userId = $_SESSION['id'];
     $id = $res['lastId'];
     $res = safeQueryId("INSERT INTO tracking_user (idTracking, idUser) VALUES (?, ?)", array($id, $userId));
     echo json_encode(array('redirect' => array('page' => '../index.html')));
