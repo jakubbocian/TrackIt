9082c6959d694c4200106a0894dc035d201689be
ZTDavide <dadocarto503@gmail.com>
2023-05-23 11:56:16 +0200

    Miglioramenti colori/menu nel index.html e creazione addtracking,.php

--- tracking/addTracking.php
+++ tracking/addTracking.php
@@ -1,14 +1,132 @@
 <html>
-    <head>
-        <script src="../ajax.js"></script>
-    </head>
-    <body>
-        <form name='registra'>
-            <input type="text" name="name">
-            <input type="text" name="carrier">
-            <input type="text" name="tracking">
-            <input type="button" name="submit" onclick="POST('controller.php', 'registra', 'error')" value="aggiungi">
-        </form>
-        <div id="error">
+
+<head>
+
+    <?php
+
+    session_start();
+    if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+
+        header('Location: ../accesso/login.php');
+        exit;
+    }
+
+    $user = $_SESSION["username"];
+
+    ?>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title> Nuovo tracciamento</title>
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='addTracking.css' rel='stylesheet'>
+
+    <script type='text/javascript' src=''></script>
+    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+    <script type='text/javascript'
+        src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+</head>
+
+<body oncontextmenu='return false' class='snippet-body'>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Logo</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../tuoipacchi.php">Tuoi pacchi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../accesso/logout.php">Esci</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </body>
+    </nav>
+
+    <div id="booking" class="section">
+        <div class="section-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-7 col-md-push-5">
+                        <div class="booking-cta">
+                            <h1>Aggiungi un pacco da tracciare</h1>
+                            <p>Tracciare i tuoi pacchi non è mai stato così comodo.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-md-pull-7">
+                        <div class="booking-form">
+                            <form>
+                                <div class="form-group">
+                                    <span class="form-label">Dai un nome al tracciamento</span>
+                                    <input class="form-control" type="text" placeholder="Che nome vorresti dare?">
+                                </div>
+
+                                <div class="form-group">
+                                    <span class="form-label">Corriere</span>
+                                    <input class="form-control" type="text"
+                                        placeholder="A quale corriere è stato assegnato?">
+                                </div>
+
+                                <div class="form-group">
+                                    <span class="form-label">Codice spedizione</span>
+                                    <input class="form-control" type="text" placeholder="Qual'è il codice del pacco?">
+                                </div>
+
+                                <!--<div class="row">
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <span class="form-label">Rooms</span>
+                                            <select class="form-control">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                            </select>
+                                            <span class="select-arrow"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <span class="form-label">Adults</span>
+                                            <select class="form-control">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                            </select>
+                                            <span class="select-arrow"></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-group">
+                                            <span class="form-label">Children</span>
+                                            <select class="form-control">
+                                                <option>0</option>
+                                                <option>1</option>
+                                                <option>2</option>
+                                            </select>
+                                            <span class="select-arrow"></span>
+                                        </div>
+                                    </div>
+                                </div>-->
+                                <div class="form-btn">
+                                    <button class="submit-btn">Avvia tracciamento</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type='text/javascript'></script>
+</body>
+
+</html>
