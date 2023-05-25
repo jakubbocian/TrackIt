2d6bde3cc34a2158d0f06ffa43d23257633ebce1
jakubbocian <jakub.bocian@mpolo.it>
2023-05-25 09:12:58 +0200

    summit

--- tracking/trackingsTemplate.php
+++ tracking/trackingsTemplate.php
@@ -48,6 +48,7 @@ function trackings()
         
         $code .= "<li>";
         $code .= "<h3>" . $row["name"] . "</h3>";
+        $code .= "<button class='delete' onclick=\"GET('controller.php?idDelete=" . $row["id"] . "','dummy', 'dummy')\">Delete</button>";
         $code .= "<div class='shipment-details'>";
         $code .= "<h2>Shipment Details</h2>";
         $code .= "<p><span>Carrier:</span> " . $row["carrier"] . "</p>";
