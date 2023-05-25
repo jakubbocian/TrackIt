7d0c23695bcd28e83e9493f73e06d67a4e5b5cc7
ZTDavide <dadocarto503@gmail.com>
2023-05-25 09:17:12 +0200

    Update trackings.php

--- tracking/trackings.php
+++ tracking/trackings.php
@@ -28,7 +28,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="../index.html">Home</a>
+                        <a class="nav-link" href="../index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="addTracking.php">Aggiungi un tracking</a>
