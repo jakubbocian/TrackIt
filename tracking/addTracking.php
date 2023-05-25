d81de8307178b54b8ce4034d1f36ad8bab49d19a
jakubbocian <jakub.bocian@mpolo.it>
2023-05-25 09:04:51 +0200

    trackings con delete

--- tracking/addTracking.php
+++ tracking/addTracking.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+
     <?php
     include_once "../session.php";
     ?>
@@ -12,16 +13,17 @@
 
     <script type='text/javascript' src=''></script>
     <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
-    <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+    <script type='text/javascript'
+        src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
     <script src='../ajax.js'></script>
     <script>
         function genCarriers() {
-            
+
             var input = document.getElementById("carrier");
             var carrier = input.value;
             console.log(carrier);
             var url = "controller.php?carriers=" + carrier;
-            GET(url+"&", "addTracking", "carriers");
+            GET(url + "&", "addTracking", "carriers");
         }
     </script>
 </head>
@@ -63,15 +65,18 @@
                     </div>
                     <div class="col-md-4 col-md-pull-7">
                         <div class="booking-form">
-                            <form name="addTracking" onsubmit="event.preventDefault();">
+                            <form name="addTracking">
                                 <div class="form-group">
                                     <span class="form-label">Dai un nome al tracciamento</span>
-                                    <input class="form-control" type="text" placeholder="Che nome vorresti dare?" name="name">
+                                    <input class="form-control" type="text" placeholder="Che nome vorresti dare?"
+                                        name="name">
                                 </div>
 
                                 <div class="form-group">
                                     <span class="form-label">Corriere</span>
-                                    <input class="form-control" type="text" placeholder="A quale corriere è stato assegnato?" name="carrier" id="carrier" list="carriers" onchange="genCarriers()">
+                                    <input class="form-control" type="text"
+                                        placeholder="A quale corriere è stato assegnato?" name="carrier" id="carrier"
+                                        list="carriers" onchange="genCarriers()">
                                     <datalist id='carriers'>
 
                                     </datalist>
@@ -79,12 +84,15 @@
 
                                 <div class="form-group">
                                     <span class="form-label">Codice spedizione</span>
-                                    <input class="form-control" type="text" placeholder="Qual'è il codice del pacco?" name="tracking">
+                                    <input class="form-control" type="text" placeholder="Qual'è il codice del pacco?"
+                                        name="tracking">
                                 </div>
                                 <div class="form-btn">
-                                    <button class="submit-btn" onclick="POST('controller.php', 'addTracking', 'response')">Avvia tracciamento</button>
+                                    <button class="submit-btn"
+                                        onclick="POST('controller.php', 'addTracking', 'dummy')">Avvia
+                                        tracciamento</button>
                                 </div>
-                                <div id="response">
+                                <div id="dummy">
 
                                 </div>
                             </form>
--- tracking/trackings.php
+++ tracking/trackings.php
@@ -1,16 +1,49 @@
 <html>
 
 <head>
+<meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title> Nuovo tracciamento</title>
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='addTracking.css' rel='stylesheet'>
+
+    <script type='text/javascript' src=''></script>
+    <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+    <script type='text/javascript'
+        src='https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js'></script>
+    <script src='../ajax.js'></script>
+    <link rel="stylesheet" href="style.css">
     <?php
     include_once "../session.php";
     ?>
-    <script src="../ajax.js"></script>
-    <link rel="stylesheet" href="style.css">
 </head>
 
 <body onload="GET('controller.php?trackings', 'dummy', 'trackings')">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="addTracking.php">Aggiungi un tracking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../accesso/logout.php">Esci</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <form name='dummy'>
     </form>
+    <div id="dummy">
+    </div>
     <div id="trackings">
     </div>
 </body>
